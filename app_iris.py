2fe844ac6c59d186990f2ccba8034e343dec1c33
valere <vmangwandjo@gmail.com>
2025-02-04 13:06:38 +0100

    load api_iris

--- app_iris.py
+++ app_iris.py
@@ -1,10 +1,49 @@
 import streamlit as st
 import requests
+import joblib
+import numpy as np
+import pandas as pd
 
 # Fonction pour envoyer les données à l'API
 def send_data_to_api(data):
-    response = requests.post("http://127.0.0.1:5000/predict", json=data)
+    response = predict(data)
     return response.json()
+
+def load_model():
+    # Charger le modèle
+    model = joblib.load('model_classifier_iris.pkl')
+    scale = joblib.load("scaler.pkl")
+
+    return model, scale
+
+def predict(data):
+    model, scale = load_model()
+    try:
+        
+        # Extraire les caractéristiques
+        sepal_length = data['sepal_length']
+        sepal_width = data['sepal_width']
+        petal_length = data['petal_length']
+        petal_width = data['petal_width']
+        
+        # Créer un tableau NumPy des caractéristiques
+        features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+
+        # Convertir en DataFrame
+        X_train = pd.DataFrame(features, columns=['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth'])
+
+        # Normalisation
+        X_train_normalized = scale.transform(X_train)
+        
+        # Faire la prédiction
+        prediction = model.predict(X_train_normalized)
+        
+        # Renvoie la prédiction
+        return prediction[0]
+        
+    except Exception as e:
+        return 
+
 
 
 st.markdown("<h1 style='text-align: center;'>PREDICTION DU TYPE DE FLEUR D'IRIS</h1>", unsafe_allow_html=True)
@@ -13,7 +52,6 @@ col1, col2 = st.columns(2)
 
 with col1:
     sepal_length = st.slider("Longueur du sépal", 0.0, 10.0, value=0.0, step=0.1)
-    sepal_length = st.number_input("Entrez la longeur du sépal", min_value=0.0, max_value=10.0, step=0.1)
 
 with col2:
     sepal_width = st.slider("Largeur du sépal", 0.0, 10.0, value=0.0, step=0.1)
@@ -40,7 +78,8 @@ if st.button("Prédire la fleur...", help="Cliquez pour envoyer les données", t
     }
     
     # Envoyer les données à l'API
-    response = send_data_to_api(data)
+    response = predict(data)
+    #model, sca = load_model()
     
     # Afficher la réponse de l'API
     st.write("<p style='font-size: 20px; font-weight: bold;'>Votre fleur semble être : <span style='color: #ff4b4b;'>", response , "</span></p>", unsafe_allow_html=True)
